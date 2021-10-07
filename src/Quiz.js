4308350bc16c6d80c866a912887f49e9a320eea4
ganesh0440 <ganeshp0440@gmail.com>
2021-10-07 15:46:37 +0530

    created quizquestion component and passing props to it

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+import QuizQuestion from "./QuizQuestion";
+
 let quizData = require("./quiz_data.json");
 
 class Quiz extends Component {
@@ -12,7 +14,7 @@ class Quiz extends Component {
   render() {
     return (
       <div className="QuizQuestion">
-        {quizData.quiz_questions[0].instruction_text}
+        <QuizQuestion quiz_question={quizData.quiz_questions} />
       </div>
     );
   }
